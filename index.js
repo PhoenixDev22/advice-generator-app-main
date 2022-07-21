d8dad4355bef620a369bda892ba7a8c6376b7a84
PhoenixDev22 <96928181+PhoenixDev22@users.noreply.github.com>
2022-07-21 10:50:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ btn.addEventListener("click", () => {
     const f = setTimeout(() => {
         btn.removeAttribute("disabled")
     }, 2000);
-    console.log("clicked")
-    getData('https://api.adviceslip.com/advice').catch(error => {
-        console.error("the catched error is ", error)
+
+    getData('https://api.adviceslip.com/advice').catch((error)=> {
+        const e = new Error(error)
+        console.log("The catched error is ", e.message)
     })
 })
 async function getData(url){
     const response = await fetch(url);
-    console.log(response)
     const data = await response.json()
     const id =  data["slip"].id
     const advice = data["slip"].advice
-    console.log(data["slip"])
+
     idSpan.textContent =`#${id}`
-    quote.textContent = `"${advice}"`
+    quote.textContent = `${advice}`
 }
